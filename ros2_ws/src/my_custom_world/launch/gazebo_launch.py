2445469a883c85554a9a69b789404635ae975ec5
Jason Koubi <jazkoubi@gmail.com>
2024-05-21 15:46:27 -0700

    launch empty world

--- ros2_ws/src/my_custom_world/launch/gazebo_launch.py
+++ ros2_ws/src/my_custom_world/launch/gazebo_launch.py
@@ -1,25 +1,3 @@
-# Create-ROS2-World
-
-# Step 1: Launch Gazebo Empty World
-
-**1. Create a ROS 2 package (if you don't already have one):**
-
-```bash
-cd /ros2_ws/src/
-ros2 pkg create my_custom_world --build-type ament_cmake
-```
-
-**2. Create a launch file:**
-
-```bash
-cd /ros2_ws/src/my_custom_world
-mkdir launch
-touch launch/gazebo_launch.py
-```
-
-**3. Edit the launch file (gazebo_launch.py):**
-
-```bash
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
@@ -44,26 +22,3 @@ def generate_launch_description():
         gazebo,
         gzclient
     ])
-```
-**4. Modify CMakeLists.txt:**
-
-```bash
-install(
-  DIRECTORY launch
-  DESTINATION share/${PROJECT_NAME}
-)
-
-```
-
-**5. Build your package and source workspace**
-
-```bash
-cd /ros2_ws
-colcon build --packages-select my_custom_world
-source /ros2_ws/install/setup.bash
-```
-**6. Launch Gazebo using the launch file:**
-
-```bash
-ros2 launch my_custom_world gazebo_launch.py
-```
